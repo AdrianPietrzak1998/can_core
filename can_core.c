6b111c1ae3502a3dd78603e222c285445715e68f
Adrian Pietrzak <adrianp126p@gmail.com>
2025-05-24 23:02:39 +0200

    Init

---
+++ can_core.c
@@ -0,0 +1,166 @@
+/*
+ * can_core.c
+ *
+ *  Created on: May 23, 2025
+ *      Author: Adrian
+ */
+
+#include "can_core.h"
+#include "assert.h"
+#include <stddef.h>
+
+#if CC_TICK_FROM_FUNC
+
+CC_TIME_t (*CC_get_tick)(void) = NULL;
+
+#define CC_GET_TICK ((CC_get_tick != NULL) ? CC_get_tick() : ((CC_TIME_t)0))
+
+void CC_tick_function_register(CC_TIME_t (*Function)(void))
+{
+	assert(Function != NULL);
+
+    CC_get_tick = Function;
+}
+
+#else
+
+CC_TIME_t *CC_tick = NULL;
+
+#define CC_GET_TICK (*(CC_tick))
+
+void CC_tick_variable_register(CC_TIME_t *Variable)
+{
+	assert(Variable != NULL);
+
+    CC_tick = Variable;
+}
+
+#endif
+
+static inline void CopyBuf(uint8_t *restrict src, uint8_t *restrict dst, size_t size)
+{
+	assert((src != NULL) || (dst != NULL));
+
+	for(size_t i; i < size; i++)
+	{
+		dst[i] = src[i];
+	}
+}
+
+void CC_RX_PushMsg(CC_RX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag)
+{
+	assert(Instance != NULL);
+
+
+	uint16_t next_head = Instance->Head + 1;
+	if (next_head >= CC_RX_BUFFER_SIZE)
+	{
+		next_head = 0;
+	}
+
+	if (next_head == Instance->Tail)
+	{
+		return;
+	}
+
+	Instance->Head = next_head;
+
+	Instance->Buf[Instance->Head].ID = ID;
+	Instance->Buf[Instance->Head].DLC = DLC;
+	Instance->Buf[Instance->Head].IDE_flag = IDE_flag;
+
+	if( (DLC > 0) && (DLC <= 8) )
+	{
+		CopyBuf(Data, Instance->Buf[Instance->Head].Data, DLC);
+	}
+
+}
+
+static inline CC_MsgRegStatus_t CC_RX_MsgFromTables(CC_RX_instance_t *Instance, CC_RX_message_t *Msg)
+{
+	assert(Instance != NULL);
+
+	if(NULL == Instance->RxTable)
+	{
+		return CC_MSG_UNREG;
+	}
+
+	for (uint16_t i = 0; i < Instance->TableSize; i++)
+	{
+		if(Instance->RxTable[i].ID == Msg->ID)
+		{
+			if( (Instance->RxTable[i].DLC == Msg->DLC) && (Instance->RxTable[i].IDE_flag == Msg->IDE_flag) )
+			{
+				Instance->RxTable[i].Parser(Instance, Msg, Instance->RxTable[i].SlotNo);
+				return CC_MSG_REG;
+			}
+		}
+	}
+	return CC_MSG_UNREG;
+}
+
+static void CC_Timeout_Check(CC_RX_instance_t *Instance)
+{
+	if(NULL != Instance->RxTable)
+	{
+		for (uint16_t i = 0; i < Instance->TableSize; i++)
+		{
+			if( (0 != Instance->RxTable[i].TimeOut) && (CC_GET_TICK - Instance->RxTable[i].LastTick >= Instance->RxTable[i].TimeOut) )
+			{
+				Instance->RxTable[i].LastTick = CC_GET_TICK;
+				assert(NULL != Instance->TimeoutCallback);
+				Instance->TimeoutCallback(Instance, Instance->RxTable[i].SlotNo);
+			}
+		}
+	}
+}
+
+void CC_RX_Poll(CC_RX_instance_t *Instance)
+{
+	assert(Instance != NULL);
+
+	CC_Timeout_Check(Instance);
+
+	while(Instance->Head != Instance->Tail)
+	{
+		Instance->Tail++;
+		if(Instance->Tail >= CC_RX_BUFFER_SIZE)
+		{
+			Instance->Tail = 0;
+		}
+
+		if((CC_RX_MsgFromTables(Instance, &Instance->Buf[Instance->Tail]) != CC_MSG_REG) && NULL != Instance->Parser_unreg_msg)
+		{
+			Instance->Parser_unreg_msg(Instance, &Instance->Buf[Instance->Tail]);
+		}
+
+	}
+}
+
+void CC_TX_PushMsg(CC_TX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag)
+{
+	assert(Instance != NULL);
+
+	uint16_t next_head = Instance->Head + 1;
+	if (next_head >= CC_RX_BUFFER_SIZE)
+	{
+		next_head = 0;
+	}
+
+	if (next_head == Instance->Tail)
+	{
+		return;
+	}
+
+	Instance->Head = next_head;
+
+	Instance->Buf[Instance->Head].ID = ID;
+	Instance->Buf[Instance->Head].DLC = DLC;
+	Instance->Buf[Instance->Head].IDE_flag = IDE_flag;
+
+	if( (DLC > 0) && (DLC <= 8) )
+	{
+		CopyBuf(Data, Instance->Buf[Instance->Head].Data, DLC);
+	}
+}
+
---
+++ can_core.h
@@ -0,0 +1,133 @@
+/*
+ * can_core.h
+ *
+ *  Created on: May 23, 2025
+ *      Author: Adrian
+ */
+
+#ifndef CAN_CORE_H_
+#define CAN_CORE_H_
+
+#include <limits.h>
+#include <stdint.h>
+
+#define CC_TICK_FROM_FUNC 0
+
+#define CC_RX_BUFFER_SIZE 32
+#define CC_TX_BUFFER_SIZE 32
+
+#ifndef CC_TIME_BASE_TYPE_CUSTOM
+
+#define CC_MAX_TIMEOUT UINT32_MAX
+typedef volatile uint32_t CC_TIME_t;
+
+#else
+
+typedef CC_TIME_BASE_TYPE_CUSTOM CC_TIME_t;
+
+#if defined(CC_TIME_BASE_TYPE_CUSTOM_IS_UINT8)
+#define CC_MAX_TIMEOUT UINT8_MAX
+#elif defined(CC_TIME_BASE_TYPE_CUSTOM_IS_UINT16)
+#define CC_MAX_TIMEOUT UINT16_MAX
+#elif defined(CC_TIME_BASE_TYPE_CUSTOM_IS_UINT32)
+#define CC_MAX_TIMEOUT UINT32_MAX
+#elif defined(CC_TIME_BASE_TYPE_CUSTOM_IS_UINT64)
+#define CC_MAX_TIMEOUT UINT64_MAX
+#elif defined(CC_TIME_BASE_TYPE_CUSTOM_IS_INT8)
+#define CC_MAX_TIMEOUT INT8_MAX
+#elif defined(CC_TIME_BASE_TYPE_CUSTOM_IS_INT16)
+#define CC_MAX_TIMEOUT INT16_MAX
+#elif defined(CC_TIME_BASE_TYPE_CUSTOM_IS_INT32)
+#define CC_MAX_TIMEOUT INT32_MAX
+#elif defined(CC_TIME_BASE_TYPE_CUSTOM_IS_INT64)
+#define CC_MAX_TIMEOUT INT64_MAX
+#else
+#error "CC_MAX_TIMEOUT: Unknown CC_TIME_BASE_TYPE_CUSTOM or missing _IS_* define"
+#endif
+
+#endif
+
+typedef enum{
+	CC_BUS_BUSY = 0,
+	CC_BUS_FREE
+}CC_BusIsFree_t;
+
+typedef enum{
+	CC_MSG_UNREG,
+	CC_MSG_REG
+}CC_MsgRegStatus_t;
+
+typedef struct {
+	uint32_t ID;
+	uint8_t Data[8];
+	uint8_t DLC : 4;
+	uint8_t IDE_flag :1;
+	CC_TIME_t Time;
+}CC_RX_message_t;
+
+typedef struct CC_RX_instance_t CC_RX_instance_t;
+typedef struct {
+	uint16_t SlotNo;
+	uint32_t ID;
+	uint8_t DLC : 4;
+	uint8_t IDE_flag :1;
+	CC_TIME_t TimeOut;
+	void (*Parser)(const CC_RX_instance_t *Instance, CC_RX_message_t *Msg, uint16_t Slot);
+	CC_TIME_t LastTick;
+}CC_RX_table_t;
+
+struct CC_RX_instance_t
+{
+	CC_RX_message_t Buf[CC_RX_BUFFER_SIZE];
+	uint16_t Head, Tail;
+	CC_RX_table_t *RxTable;
+	uint16_t TableSize;
+	void (*Parser_unreg_msg)(const CC_RX_instance_t *Instance, CC_RX_message_t *Msg);
+	void (*TimeoutCallback)(CC_RX_instance_t *Instance, uint16_t Slot);
+};
+
+
+
+
+typedef struct {
+	uint32_t ID;
+	uint8_t Data[8];
+	uint8_t DLC : 4;
+	uint8_t IDE_flag :1;
+}CC_TX_message_t;
+
+typedef struct CC_TX_instance_t CC_TX_instance_t;
+typedef struct
+{
+	uint32_t ID;
+	uint8_t *Data;
+	uint8_t DLC : 4;
+	uint8_t IDE_flag :1;
+	CC_TIME_t TimeOut;
+	CC_TIME_t LastTick;
+}CC_TX_table_t;
+
+struct CC_TX_instance_t
+{
+	CC_TX_message_t Buf[CC_TX_BUFFER_SIZE];
+	uint16_t Head, Tail;
+	CC_RX_table_t *TxTable;
+	uint16_t TableSize;
+	void (*SendFunction)(const CC_TX_instance_t *Instance, const CC_TX_message_t *msg);
+	CC_BusIsFree_t (*BusCheck)(const CC_TX_instance_t *Instance);
+};
+
+// Tick source registration
+#if CC_TICK_FROM_FUNC
+void CC_tick_function_register(CC_TIME_t (*Function)(void));
+#else
+void CC_tick_variable_register(CC_TIME_t *Variable);
+#endif
+
+void CC_RX_PushMsg(CC_RX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag);
+void CC_RX_Poll(CC_RX_instance_t *Instance);
+
+void CC_TX_PushMsg(CC_TX_instance_t *Instance, uint32_t ID, uint8_t *Data, uint8_t DLC, uint8_t IDE_flag);
+void CC_TX_Poll(CC_TX_instance_t *Instance);
+
+#endif /* CAN_CORE_H_ */
